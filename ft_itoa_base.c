f0ff1fa6cf818cd1bfa8396a0f072b7c626eeb96
Gonçalo Pedroso <45522361+Pedroso221@users.noreply.github.com>
2025-02-03 22:42:25 +0000

    Add files via upload

--- ft_itoa_base.c
+++ ft_itoa_base.c
@@ -12,30 +12,40 @@
 
 #include "ft_printf.h"
 
-char	*ft_itoa_base(unsigned long n, int base, int uppercase)
+static int	fill_buffer(char *buffer, unsigned long n, int base, int uppercase)
 {
-	char		*digits;
-	char		buffer[17];
-	char		*result;
-	int			len;
+	char	*digits;
+	int		len;
 
 	if (uppercase)
 		digits = "0123456789ABCDEF";
 	else
 		digits = "0123456789abcdef";
 	len = 0;
-	//if (n == 0)
-		//return (strdup("0"));
+	if (n == 0)
+		buffer[len++] = '0';
 	while (n)
 	{
 		buffer[len++] = digits[n % base];
 		n /= base;
 	}
+	return (len);
+}
+
+char	*ft_itoa_base(unsigned long n, int base, int uppercase)
+{
+	char	buffer[17];
+	char	*result;
+	int		len;
+	int		i;
+
+	len = fill_buffer(buffer, n, base, uppercase);
 	result = malloc(len + 1);
 	if (!result)
 		return (NULL);
 	result[len] = '\0';
-	while (--len >= 0)
-		result[len] = buffer[len];
+	i = 0;
+	while (len--)
+		result[i++] = buffer[len];
 	return (result);
 }
